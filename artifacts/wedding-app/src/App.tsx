79e08e3d3d25f408a32566d85db99cfdb8090fc0
Davide <barnodepro@gmail.com>
2026-04-09 15:05:24 +0200

    Remove install app prompt from all routes

--- artifacts/wedding-app/src/App.tsx
+++ artifacts/wedding-app/src/App.tsx
@@ -3,7 +3,6 @@ import { Switch, Route, Router as WouterRouter, useLocation } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import InstallAppPrompt from "@/components/InstallAppPrompt";
 import NotFound from "@/pages/not-found";
 import Intro from "@/pages/Intro";
 import Home from "@/pages/Home";
@@ -198,7 +197,6 @@ function App() {
           <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
             <BackgroundMusicPlayer />
             <Router />
-            <InstallAppPrompt />
           </WouterRouter>
         </DataSourceBootstrapper>
         <Toaster />
